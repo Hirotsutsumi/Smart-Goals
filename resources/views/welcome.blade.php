b365cc4745c79188b1153dca87f185422e578f24
jiro.kirameki <ec2-user@ip-172-31-29-130.us-east-2.compute.internal>
2018-07-19 05:20:02 +0000

    toppage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,67 +10,84 @@
     <h1 class="site-title">     Welcome to SmartGoals</h1>
      
     <div class="buttons">
-      <a class="button button-showy" href="{{ route('signup.get') }}">SIGN UP</a>
+      <a class="button button-showy" href="#howtouse">HOW TO USE</a>
     </div>
   </section>
   
-  <!--<section class="about" id="about">-->
-    <!--<h2 class="heading">ABOUT THIS PRODUCT</h2>-->
-    <!--<p class="about-text">-->
-    <!-- みなさんは毎日どのようなSmartGoalを設定していますか？<br>-->
-  　　<!--毎日違うスマートゴールを考えるのが億劫、同期がどんなゴールを目標にしているのか気になる…<br>-->
-  　　<!--そんなあなたの悩みを解決するアプリケーション-->
-    <!--</p>-->
-    <!--<p class="about-text">-->
-    <!--  このアプリは皆さんの目標設定と目標管理をお手伝いさせていただくツールです。<br>-->
-    <!--  このアプリを使うことで毎日の日報制作が効率的に、また、生産性のある目標を立てることができます。-->
-    <!--</p>-->
-  <!--</section>-->
 
-  <section class="skills">
+  <section class="skills" id="howtouse">
     <h2 class="heading">HOW TO USE</h2>
-    <h3><strong>スマートゴールを追加したら．．．</strong></h3>
+    
+        <h3 class="Judy"><strong>初めての方はこちら</strong></h3>
+  
     <div class="skills-wrapper">
-      <div class="skill-box">
-        
+        <div class="skill-box">
+          <img src="{{ secure_asset("images/Temp.png") }}" alt="howtouse">
+        </div>
+        <div class="skill-box">  
+         <div class="skill-title">STEP0. REGISTER</div>
+          <p class="skill-text">
+             まず今日のSmartGoalを登録しましょう。<br>
+             目標達成してきのこを集めよう！<br>
+             
+          </p>
+          <hr>
+         </div>
+       </div>
+    </section>
+    
+　<section class="skills">　　　
+        <h3 class="Judy"><strong>2回目以降の方はこちら</strong></h3>
+   
+    <div class="skills-wrapper">
+      <div class="skill-box">  
         <div class="skill-title">STEP1. REVIEW</div>
         <p class="skill-text">
-          前日のSmartGoalを振り返り<br>
+          今日のSmartGoalを振り返り
           達成率などを入力<br>
           出力されたテンプレートで日報を作成しましょう！
         </p>
-    
       </div>
-      
-       <div class="skill-box">
-        <span class = "glyphicon glyphicon-arrow-right" style="font-size:50px" area-hidden="true">
-        </span>
-       </div>   
-      
-      <div class="skill-box">
-        <div class="skill-title">STEP2. CHECK</div>
+        <div class="skill-box">
+          <img src="{{ secure_asset("images/Temp.png") }}" alt="howtouse">
+        </div>
+     </div>
+  </section>
+  
+  <section class="skills">
+       <div class="skills-wrapper">
+        <div class="skill-box">
+          <img src="{{ secure_asset("images/Temp.png") }}" alt="howtouse">
+        </div>
+        <div class="skill-box">  
+         <div class="skill-title">STEP2. CHECK</div>
+          <p class="skill-text">
+             過去の達成度と
+             同期のSmartGoalを参考に<br>
+             明日のSmartGoalを考えよう！
+          </p>
+         </div>
+       </div>
+  </section>
+  
+  <section class="skills">
+      <div class="skills-wrapper">
+     <div class="skill-box">  
+        <div class="skill-title">STEP3. ADD</div>
         <p class="skill-text">
-        過去の達成度と<br>
-        同期のSmartGoalを参考に<br> 
-         今日のSmartGoalを<br>
-         考えよう！
-        </p>
-      </div>
-   <div class="skill-box">
-         <span class = "glyphicon glyphicon-arrow-right" style="font-size:50px" area-hidden="true">
-         </span>
-   </div>  
-       
-        <div class="skill-title">SREP3. ADD</div>
-        <p class="skill-text">
-         SmartGoalを追加して<br>
-          スムーズに日報作成<br>
+          SmartGoalを追加してスムーズに日報作成<br>
           出力されたテンプレートで日報完成！
-         
         </p>
       </div>
+        <div class="skill-box">
+          <img src="{{ secure_asset("images/Temp.png") }}" alt="howtouse">
+        </div>
+      </div>
+      <div class="buttons">
+      <a class="button button-showyy" href="{{ route('signup.get') }}">SIGN UP!</a>
     </div>
   </section>
+  
   <section class="contact" id="contact">
     <h2 class="heading">ABOUT US</h2>
      <p class="contact-text">
