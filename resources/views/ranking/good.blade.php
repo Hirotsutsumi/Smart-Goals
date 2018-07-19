9ae3dcdc1d365c4685bda2becd3e4c73ea65cc7b
ryota.nagatomo <ec2-user@ip-172-31-46-39.us-east-2.compute.internal>
2018-07-19 05:17:58 +0000

    list if

--- resources/views/ranking/good.blade.php
+++ resources/views/ranking/good.blade.php
@@ -4,6 +4,7 @@
     <div class='page_title'>
         <h1>Smart Goals List</h1>
     </div>
+    @if(count($mygoals) >0)
     <div class="col-sm-6">
         <div class="panel panel-primary" style="word-break:break-all;">
             <div class="panel-heading">
@@ -12,6 +13,8 @@
             @include('ranking.index', ['goals' => $mygoals])
         </div>
     </div>
+    @endif
+    @if(count($othersgoals) >0)
     <div class="col-sm-6">
         <div class="panel panel-primary" style="word-break:break-all;">
             <div class="panel-heading">
@@ -20,4 +23,5 @@
             @include('ranking.index', ['goals' => $othersgoals])
         </div>
     </div>
+    @endif
 @endsection
